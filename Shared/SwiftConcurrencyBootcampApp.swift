830d74ce6cc736bbb586d95be1e9d955ca1a02b9
Tim <74768819+kodegut@users.noreply.github.com>
2022-06-07 15:20:38 +0200

    Initial Commit

---
+++ Shared/SwiftConcurrencyBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyBootcampApp.swift
+//  Shared
+//
+//  Created by Tim Musil on 07.06.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
